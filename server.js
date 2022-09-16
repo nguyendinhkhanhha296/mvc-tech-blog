04e0223f6f7521098e3b5744b01b26b999382db6
nguyendinhkhanhha296 <nguyendinhkhanhha296@gmail.com>
2022-09-15 21:47:50 -0700

    create auth and change date format

--- server.js
+++ server.js
@@ -14,24 +14,30 @@ const PORT = process.env.PORT || 3001;
 
 //setup session
 const sess = {
-  secret: 'santababy',
-  cookie: { maxAge: 300000},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
+    secret: 'santababy',
+    cookie: {
+        maxAge: 300000
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
 };
 
 //handlebars initialization
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({
+    helpers
+});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session(sess));
@@ -39,6 +45,8 @@ app.use(session(sess));
 //use routes
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Now Listening on ${PORT}`));
+sequelize.sync({
+    force: false
+}).then(() => {
+    app.listen(PORT, () => console.log(`Now Listening on ${PORT}`));
 });
