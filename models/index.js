78a31509876317f222d3120df6af389ea43d4df3
nguyendinhkhanhha296 <nguyendinhkhanhha296@gmail.com>
2022-09-19 21:21:50 -0700

    Ãupdate

--- models/index.js
+++ models/index.js
@@ -7,25 +7,38 @@ User.hasMany(Post, {
     foreignKey: 'user_id'
 });
 
+//Post-User relationship
 Post.belongsTo(User, {
-    foreignKey: 'user_id',
+    foreignKey: 'user_id'
 });
 
+// Comment-User relationship
 Comment.belongsTo(User, {
-    foreignKey: 'user_id'
-  });
-  
-Comment.belongsTo(Post, {
-    foreignKey: 'post_id'
-});
-  
-User.hasMany(Comment, {
-    foreignKey: 'user_id'
-});
-  
-Post.hasMany(Comment, {
-    foreignKey: 'post_id'
+    foreignKey: 'user_id',
+    onDelete: 'cascade',
+    hooks:true
 });
 
+// Comment-Post relationship
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'cascade',
+    hooks: true
+});
+
+// User-Comment relationsihp
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'cascade',
+    hooks:true
+});
+
+// Post-Comment relationship
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'cascade',
+    hooks:true
+})
+  
 module.exports = {User, Post, Comment};
 
